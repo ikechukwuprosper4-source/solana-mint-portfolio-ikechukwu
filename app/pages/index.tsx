e445d3276634213a51b555c7b63f43322dfd1348
ikechukwuprosper4-source <ikechukwuprosper4@gmail.com>
2026-03-27 01:50:00 +0100

    Create home page component in index.tsx

---
+++ app/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Solana Mint Portfolio</h1>
+            <p>Your one-stop solution for managing Solana mints!</p>
+        </div>
+    );
+};
+
+export default HomePage;
